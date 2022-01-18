3ee3289f7c8f7ea127b02b7fb37b5a25691df923
Michał Mazur <michmzr@gmail.com>
2022-01-18 07:23:10 +0100

    Spring boot init

---
+++ src/main/java/eu/cybershu/springHelmDemo/SpringHelmDemoApplication.java
@@ -0,0 +1,13 @@
+package eu.cybershu.springHelmDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHelmDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHelmDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/cybershu/springHelmDemo/SpringHelmDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.cybershu.springHelmDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHelmDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
