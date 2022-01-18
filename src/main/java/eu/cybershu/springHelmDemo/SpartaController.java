0ff232360d3b5baa9e3c27a274f1caafe273e777
Michał Mazur <michmzr@gmail.com>
2022-01-18 07:39:39 +0100

    Helm service created Added Hello endpoint

---
+++ src/main/java/eu/cybershu/springHelmDemo/SpartaController.java
@@ -0,0 +1,19 @@
+package eu.cybershu.springHelmDemo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/")
+public class SpartaController {
+    private final String hello;
+
+    public SpartaController(@Value("${config.hello}") String hello) {
+        this.hello = hello;
+    }
+
+    @GetMapping("hello")
+    public String hello() {
+        return hello;
+    }
+}
