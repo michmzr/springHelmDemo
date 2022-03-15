55c9ddba884535a3b86953ddd258c39d9f4b5dcc
Michał Mazur <michmzr@gmail.com>
2022-03-15 07:25:51 +0100

    Upgrades

--- src/main/java/eu/cybershu/shd/SpringHelmDemoApplication.java
+++ src/main/java/eu/cybershu/shd/SpringHelmDemoApplication.java
@@ -1,13 +1,15 @@
 package eu.cybershu.shd;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class SpringHelmDemoApplication {
 
 	public static void main(String[] args) {
+		log.info("Welcome to Spring Helm Demo app!!");
 		SpringApplication.run(SpringHelmDemoApplication.class, args);
 	}
-
 }
